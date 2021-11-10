728f73b6751e9a3ada584114ec6dca64ba868071
Melanie <melanie.guerif@outlook.fr>
2021-11-10 15:33:03 +0100

    Exercice 6 finit

---
+++ Exercice6/assets/JS/script.js
@@ -0,0 +1,10 @@
+var button = document
+    .getElementById('button')
+    .addEventListener('click', function resultat (){
+
+        var shoeSize = document.getElementById('shoeSize').value;
+        var birthYear = document.getElementById('birthYear').value;
+
+        alert('Résultat : ' + ((shoeSize / birthYear) + 5));
+    })
+    
