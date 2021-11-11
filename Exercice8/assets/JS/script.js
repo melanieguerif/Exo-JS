1383df4831642e01e4e637d436322ba30dedc7a8
Melanie <melanie.guerif@outlook.fr>
2021-11-11 19:09:24 +0100

    Exercice 8 finit

---
+++ Exercice8/assets/JS/script.js
@@ -0,0 +1,10 @@
+document.getElementById('img')
+        .addEventListener("mouseenter" , function changeImg(){
+            document.getElementById("img").src="img/Chat.jpg"
+        });
+
+document.getElementById('img')
+        .addEventListener("mouseleave" , function changImgAgain(){
+            document.getElementById("img").src="img/Tigre.jpg"
+        });
+
