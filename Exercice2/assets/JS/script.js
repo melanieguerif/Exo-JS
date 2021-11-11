4ff96391aa72920aa9b8840d16d0af6bb1d92cd3
Melanie <melanie.guerif@outlook.fr>
2021-11-11 11:35:07 +0100

    Exercice 2mis à jour

--- Exercice2/assets/JS/script.js
+++ Exercice2/assets/JS/script.js
@@ -2,4 +2,4 @@ var lastName = 'Doe';
 var firstName = 'John';
 var city = 'New-York';
 
-alert('Nom : ' +  lastName + "\n Prénom : " + firstName + '\n Ville : ' + city);
+alert('Nom : ' +  lastName + "\nPrénom : " + firstName + '\nVille : ' + city);
