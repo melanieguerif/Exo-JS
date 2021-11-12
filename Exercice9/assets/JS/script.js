d27bbf0026f4130e902c6bb266d4396cc3e8787f
Melanie <melanie.guerif@outlook.fr>
2021-11-12 08:45:38 +0100

    Exercice 9 finit

---
+++ Exercice9/assets/JS/script.js
@@ -0,0 +1,5 @@
+var img = document.getElementById('img')
+                  .addEventListener("click", function imgAgrandir(){
+                    document.getElementById('img').style.height = "30%" ;
+                    document.getElementById('img').style.width = "30%" ;
+                  });
